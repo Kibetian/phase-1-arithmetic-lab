d3f906419e07951e5f272110f18ea0991ae9d0ec
Your Name <iankibet87@gmail.com>
2023-07-02 15:39:11 +0800

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//  we get a random decimal number between 0 (inclusive) and Number.MAX_SAFE_INTEGER (exclusive).
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 17;
+let num4 = 13;
+let mod = num3 % num4;
+
+let numbers = [10, 5, 20, 15, 8];
+let max = Math.max(...numbers);
